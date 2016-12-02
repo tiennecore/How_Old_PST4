f0bbe78f5d98ae7f39a155912ee2e6df0f76d9c8
EISAS <eisas@MacBook-de-EISAS.local>
2016-12-02 15:07:35 +0100

    first

---
+++ PST/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  
+//
+//  Created by EISAS on 02/12/2016.
+//
+//
+
+import Foundation
--- PST/ViewController.swift
+++ PST/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var score_total: UILabel!
+    @IBOutlet weak var valeur_attribué: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func menu(_ sender: AnyObject) {
+    }
 
+    @IBAction func setting(_ sender: AnyObject) {
+    }
+
+    
+    @IBAction func bar_age(_ sender: UISlider) {
+        valeur_attribué.text = String(Int(sender.value))
+    }
+    
+    
+    @IBAction func validation_button(_ sender: AnyObject) {
+    }
+    
+    
+    
+    
 }
 
